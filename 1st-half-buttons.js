709a05e3c7b752628f3cd887620b788d4357f490
Yerim Lee <ylee@Yerims-MacBook-Pro.local>
2019-12-18 23:16:24 -0500

    updates

--- 1st-half-buttons.js
+++ 1st-half-buttons.js
@@ -49,12 +49,22 @@ $(document).ready(function() {
   $('#sect5-next').click(function() {
   //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-5").removeClass("visible");
-  $(".sect-6").addClass("visible");
+  $(".sect-5-1").addClass("visible");
   });
   $('#sect5-back').click(function() {
   //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-5").removeClass("visible");
   $(".sect-4").addClass("visible");
+  });
+  $('#sect5-1-next').click(function() {
+  //ID or Class name of the modal you want to appear on click goes into this second area.
+  $(".sect-5-1").removeClass("visible");
+  $(".sect-6").addClass("visible");
+  });
+  $('#sect5-1-back').click(function() {
+  //ID or Class name of the modal you want to appear on click goes into this second area.
+  $(".sect-5-1").removeClass("visible");
+  $(".sect-5").addClass("visible");
   });
   $('#sect6-next').click(function() {
   //ID or Class name of the modal you want to appear on click goes into this second area.
@@ -64,7 +74,7 @@ $(document).ready(function() {
   $('#sect6-back').click(function() {
   //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-6").removeClass("visible");
-  $(".sect-5").addClass("visible");
+  $(".sect-5-1").addClass("visible");
   });
   $('#sect7-next').click(function() {
   //ID or Class name of the modal you want to appear on click goes into this second area.
