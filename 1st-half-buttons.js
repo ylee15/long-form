bd2f55e3d51e8277bcee85e0047034601a005834
Caroline Guirgis <cguirgis@nd.edu>
2019-12-19 19:52:19 -0500

    long-form

--- 1st-half-buttons.js
+++ 1st-half-buttons.js
@@ -1,239 +1,246 @@
 $(document).ready(function() {
-  //ID or Class name of the button goes into the first section
   $('#title-next').click(function() {
-    //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".sect-1").addClass("visible");
     $(".title").removeClass("visible");
   });
+    
   $('#sect1-next').click(function() {
-    //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".sect-1").removeClass("visible");
     $(".sect-2").addClass("visible");
     $("#sect2-next").addClass("jordan");
   });
+    
   $('#sect1-back').click(function() {
-    //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".sect-1").removeClass("visible");
     $(".title").addClass("visible");
   });
+    
   $('#sect2-back').click(function() {
-    //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".sect-2").removeClass("visible");
     $(".sect-1").addClass("visible");
   });
+    
     $('#sect2-next').click(function() {
-    //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".sect-2").removeClass("visible");
     $(".sect-3").addClass("visible");
   });
+    
     $('#sect3-back').click(function() {
-    //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".sect-3").removeClass("visible");
     $(".sect-2").addClass("visible");
   });
+    
   $('#sect3-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-3").removeClass("visible");
   $(".sect-4").addClass("visible");
   });
+    
   $('#sect4-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-4").removeClass("visible");
   $(".sect-5").addClass("visible");
   });
+    
   $('#sect4-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-4").removeClass("visible");
   $(".sect-3").addClass("visible");
   });
+    
   $('#sect5-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-5").removeClass("visible");
   $(".sect-5-1").addClass("visible");
   });
+    
   $('#sect5-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-5").removeClass("visible");
   $(".sect-4").addClass("visible");
   });
+    
   $('#sect5-1-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-5-1").removeClass("visible");
   $(".sect-6").addClass("visible");
   });
+    
   $('#sect5-1-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-5-1").removeClass("visible");
   $(".sect-5").addClass("visible");
   });
+    
   $('#sect6-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-6").removeClass("visible");
   $(".sect-7").addClass("visible");
   });
+    
   $('#sect6-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-6").removeClass("visible");
   $(".sect-5-1").addClass("visible");
   });
+    
   $('#sect7-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-7").removeClass("visible");
   $(".sect-8").addClass("visible");
   });
+    
   $('#sect7-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-7").removeClass("visible");
   $(".sect-6").addClass("visible");
   });
+    
   $('#sect8-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-8").removeClass("visible");
   $(".sect-9").addClass("visible");
   });
+    
   $('#sect8-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-8").removeClass("visible");
   $(".sect-7").addClass("visible");
   });
+    
   $('#sect9-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-9").removeClass("visible");
   $(".sect-10").addClass("visible");
   });
+    
   $('#sect9-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-9").removeClass("visible");
   $(".sect-8").addClass("visible");
   });
+    
   $('#sect10-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-10").removeClass("visible");
   $(".sect-11").addClass("visible");
   });
+    
   $('#sect10-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-10").removeClass("visible");
   $(".sect-9").addClass("visible");
   });
+    
   $('#sect11-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-11").removeClass("visible");
   $(".sect-12").addClass("visible");
   });
+    
   $('#sect11-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-11").removeClass("visible");
   $(".sect-10").addClass("visible");
   });
+    
   $('#sect12-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-12").removeClass("visible");
   $(".sect-13").addClass("visible");
   });
+    
   $('#sect12-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-12").removeClass("visible");
   $(".sect-11").addClass("visible");
   });
+    
   $('#sect13-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-13").removeClass("visible");
   $(".sect-14").addClass("visible");
   });
+    
   $('#sect13-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-13").removeClass("visible");
   $(".sect-12").addClass("visible");
   });
+    
   $('#sect14-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-14").removeClass("visible");
   $(".sect-15").addClass("visible");
   });
+    
   $('#sect14-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-14").removeClass("visible");
   $(".sect-13").addClass("visible");
   });
   $('#sect15-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-15").removeClass("visible");
   $(".sect-16").addClass("visible");
   });
+    
   $('#sect15-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-15").removeClass("visible");
   $(".sect-14").addClass("visible");
   });
+    
   $('#sect16-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-16").removeClass("visible");
   $(".sect-17").addClass("visible");
   });
+    
   $('#sect16-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-16").removeClass("visible");
   $(".sect-15").addClass("visible");
   });
+    
   $('#sect17-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-17").removeClass("visible");
   $(".sect-18").addClass("visible");
   });
+    
   $('#sect17-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-17").removeClass("visible");
   $(".sect-16").addClass("visible");
   });
+    
   $('#sect18-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-18").removeClass("visible");
   $(".sect-19").addClass("visible");
   });
+    
   $('#sect18-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-18").removeClass("visible");
   $(".sect-17").addClass("visible");
   });
+    
   $('#sect19-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-19").removeClass("visible");
   $(".sect-20").addClass("visible");
   });
+    
   $('#sect19-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-19").removeClass("visible");
   $(".sect-18").addClass("visible");
   });
+    
   $('#sect20-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-20").removeClass("visible");
   $(".sect-19").addClass("visible");
   });
+    
   $('#sect20-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-20").removeClass("visible");
   $(".sect-21").addClass("visible");
   });
+    
   $('#sect21-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-21").removeClass("visible");
   $(".sect-20").addClass("visible");
   });
+    
   $('#sect21-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-21").removeClass("visible");
   $(".sect-22").addClass("visible");
   });
+    
   $('#sect22-back').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-22").removeClass("visible");
   $(".sect-21").addClass("visible");
   });
+    
   $('#sect22-next').click(function() {
-  //ID or Class name of the modal you want to appear on click goes into this second area.
   $(".sect-22").removeClass("visible");
   $(".sect-23").addClass("visible");
   });
+    
+  $('#sect23-back').click(function() {
+  $(".sect-23").removeClass("visible");
+  $(".sect-22").addClass("visible");
+  });
+    
+  $('#sect23-next').click(function() {
+  $(".sect-23").removeClass("visible");
+  $(".sect-23").addClass("visible");
+  });   
 });
